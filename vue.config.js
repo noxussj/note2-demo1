9acb4749ea144e5f8c4c5e005fe99ee39bcef0db
noxussj <632922356@qq.com>
2023-02-02 09:53:52 +0000

    update

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  // 兼容在线 vscode，平时无需添加
+  devServer: {
+    host: '0.0.0.0',
+    allowedHosts: ['localhost', '.gitpod.io'],
+  },
 })
